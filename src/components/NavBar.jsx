40d528c1fbaf061499881d601c40aa1bea69d779
hussain-shariff <hussainowaiz070@gmail.com>
2023-04-06 19:56:52 +0530

    navbar backdrop

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div className=' z-20 sticky top-0 bg-black/30 drop-shadow-xl text-white flex items-center
+    <div className=' z-20 sticky top-0 bg-black/30 backdrop-blur-lg text-white flex items-center
     px-10 py-3 md:px-20 border-b-[1px] border-gray-700'>
         <Link to={'/'}className='flex-grow'>
           <h1 className='text-2xl md:text-3xl font-bold font-mono cursor-pointer'>quiz app.</h1>
